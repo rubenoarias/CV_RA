7aead83afa9685754f028b6acf5c90e041e7b4b4
RUBEN <inicio3d.ar@gmail.com>
2023-03-20 20:13:57 -0300

    correccion de trabajo

--- javascript.js
+++ javascript.js
@@ -7,31 +7,3 @@ function objetivo() {
         content.style.display = "none";
     }
 }
-//Estudios
-function lupa(){
-    var onmouseover= void (document.getElementById('lupa').style.fontSize='3em')
-    var onmouseout= void(document.getElementById('lupa').style.fontSize='1em')
-}
-
-//contacto
-const form = document.querySelector('#contact-form');
-const nameInput = document.querySelector('#name');
-const lastnameInput = document.querySelector('#lastname');
-const emailInput = document.querySelector('#email');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // Aquí podrías enviar los datos del formulario a través de AJAX o fetch
-  console.log({
-    name: nameInput.value,
-    lastname: lastnameInput.value,
-    email: emailInput.value
-  });
-});
-
-const cancelBtn = document.querySelector('#cancel-btn');
-cancelBtn.addEventListener('click', () => {
-  form.reset();
-});
-
